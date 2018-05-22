ffc1bc4b6483fc4f73e35d0a06da2e335fc99b98
Tina Giska <tina.tylicka@gmail.com>
2018-05-22 22:12:35 +0200

    Create task

---
+++ server.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var app = express();
+
+app.use(express.static('views'));
+
+app.set('view engine', 'pug');
+app.set('views','./views');
+
+app.use('/store', function(req, res, next){
+    console.log('Jestem pośrednikiem przy żądaniu do /store');
+    next();
+});
+
+
+app.get('/dynamic', function(req, res){
+    res.render('dynamic', {
+        instruction: "Zaloguj się"
+    });
+});
+
+app.listen(3000);
+app.use(function (req, res, next) {
+    res.status(404).send('Wybacz, nie mogliśmy odnaleźć tego, czego żądasz!')
+});
