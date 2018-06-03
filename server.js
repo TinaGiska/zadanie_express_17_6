37b9016051019072affa1e153712acb8cfeef10f
Tina Giska <tina.tylicka@gmail.com>
2018-06-03 22:36:08 +0200

    Update task

--- server.js
+++ server.js
@@ -18,6 +18,12 @@ app.get('/dynamic', function(req, res){
     });
 });
 
+app.get('/auth/google', function(req, res){
+    res.render('google', {
+      instruction: "Jesteś zalogowany!"
+    });
+});
+
 app.listen(3000);
 app.use(function (req, res, next) {
     res.status(404).send('Wybacz, nie mogliśmy odnaleźć tego, czego żądasz!')
